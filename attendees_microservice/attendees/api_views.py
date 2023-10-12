6f8154da12ecb972e96ae08a7ad8a1d803bb53e4
Anthony Spence <anthonyspence08091973@gmail.com>
2023-10-12 11:19:16 -0700

    react running

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -67,6 +67,7 @@ def api_list_attendees(request, conference_vo_id=None):
         # Get the Conference object and put it in the content dict
         try:
             conference_href = content["conference"]
+            href = f"/api/conferences/{conference_vo_id}/" 
             conference = ConferenceVO.objects.get(import_href=conference_href)
             content["conference"] = conference
         except ConferenceVO.DoesNotExist:
